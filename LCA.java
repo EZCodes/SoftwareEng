4a8d1339e40ba83551c9433dfa44a3d3bb1a7514
George <38333392+EZCodes@users.noreply.github.com>
2019-09-18 12:48:56 +0100

    Add stubs to work on

---
+++ LCA.java
@@ -0,0 +1,18 @@
+
+public class LCA {
+	
+	public class Tree {
+		Node root;
+		
+		public class Node {
+			int value;
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
