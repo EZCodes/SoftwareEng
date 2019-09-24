03974067049a2b0d5dc7ff486e30745858c2c2b8
Jerzy Jaskuc <jaskucj@tcd.ie>
2019-09-24 20:26:01 +0100

    fixed rest of the build bugs, running tests

---
+++ LCA_test.go
@@ -0,0 +1,219 @@
+package LCA
+
+import "testing"
+
+func TestLowestCommonAncestor(t *testing.T)  {
+
+	tests := []struct {
+		tree    Tree
+		nodeOne Node
+		nodeTwo Node
+		want    Node
+		wantErr bool
+	}{
+		{
+			// empty tree
+			nodeOne: Node{},
+			nodeTwo: Node{},
+			wantErr: true,
+		}, {
+			// line tree
+			tree: Tree{
+				root: Node{
+					data: 1,
+					childs: []*Node{
+							&Node{
+								data: 2,
+								childs: []*Node{
+										&Node{
+											data: 3,
+											childs: []*Node{
+													&Node{
+														data: 4,
+														childs: []*Node{
+																&Node{
+																	data:   5,
+																	childs: []*Node{},
+																},
+														},
+													},
+											},
+										},
+								},
+							},
+					},
+				},
+			},
+			nodeOne: Node{
+				data: 3,
+			},
+			nodeTwo: Node{
+				data: 4,
+			},
+			wantErr: false,
+			want: Node{
+				data: 2,
+			},
+		}, {
+			// two sides of the root
+			tree: Tree{
+				root: Node{
+					data: 1,
+					childs: []*Node{
+							&Node{
+								data: 4,
+								childs: []*Node{
+										&Node{
+											data:   5,
+											childs: nil,
+										},
+								},
+							},
+							&Node{
+								data: 2,
+								childs: []*Node{
+										&Node{
+											data:   3,
+											childs: nil,
+										},
+								},
+							},
+					},
+				},
+			},
+			nodeOne: Node{
+				data: 3,
+			},
+			nodeTwo: Node{
+				data: 5,
+			},
+			wantErr: false,
+			want: Node{
+				data: 1,
+			},
+		}, {
+			//one side of the root
+			tree: Tree{
+				root: Node{
+					data: 1,
+					childs: []*Node{
+							&Node{
+								data:   4,
+								childs: nil,
+							},
+							&Node{
+								data: 2,
+								childs: []*Node{
+										&Node{
+											data: 3,
+											childs: []*Node{
+													&Node{
+														data:   5,
+														childs: nil,
+													},
+											},
+										},
+										&Node{
+											data:   6,
+											childs: nil,
+										},
+								},
+							},
+					},
+				},
+			},
+			nodeOne: Node{
+				data: 5,
+			},
+			nodeTwo: Node{
+				data: 6,
+			},
+			wantErr: false,
+			want: Node{
+				data: 2,
+			},
+		}, {
+			// more than 2 childs
+			tree: Tree{
+				root: Node{
+					data: 1,
+					childs: []*Node{
+							&Node{
+								data: 2,
+								childs: []*Node{
+										&Node{
+											data:   3,
+											childs: nil,
+										},
+										&Node{
+											data:   4,
+											childs: nil,
+										},
+										&Node{
+											data:   5,
+											childs: nil,
+										},
+								},
+							},
+					},
+				},
+			},
+			nodeOne: Node{
+				data: 3,
+			},
+			nodeTwo: Node{
+				data: 4,
+			},
+			wantErr: false,
+			want: Node{
+				data: 2,
+			},
+		}, {
+			//node is not in the tree
+			tree: Tree{
+				root: Node{
+					data: 1,
+					childs: []*Node{
+							&Node{
+								data: 4,
+								childs: []*Node{
+										&Node{
+											data:   5,
+											childs: nil,
+										},
+								},
+							},
+							&Node{
+								data: 2,
+								childs: []*Node{
+										&Node{
+											data:   3,
+											childs: nil,
+										},
+								},
+							},
+					},
+				},
+			},
+			nodeOne: Node{
+				data: 3,
+			},
+			nodeTwo: Node{
+				data: 7,
+			},
+			wantErr: false,
+		}, //{
+		// 3 descendants TODO
+
+		//},
+	}
+	for _, test := range tests {
+		fNode, err := test.tree.LowestCommonAncestor(&(test.nodeOne), &(test.nodeTwo))
+		if !(test.wantErr) && err != nil {
+			t.Errorf("Error has occured: %q", err)
+		}
+		if !(test.wantErr) && test.want.data != fNode.data {
+			t.Errorf("Found ancestor is incorrect. Want: %d, Got: %d", test.want.data, fNode.data)
+		}
+	}
+}
--- LCM.go
+++ LCM.go
@@ -2,40 +2,40 @@ package LCA
 
 import "fmt"
 
-type Tree struct{
+type Tree struct {
 	root Node
 }
-type Node struct{
-	data int
+type Node struct {
+	data   int
 	childs []*Node
 }
 
 // TODO make multiple nodes, also handle if given node is not in the tree
-func (t *Tree) lowestCommonAncestor(n1, n2 *Node) (Node, error) {
+func (t *Tree) LowestCommonAncestor(n1, n2 *Node) (Node, error) {
 	var p1 []Node
 	var p2 []Node
 	if n1 == nil || n2 == nil {
 		return Node{}, fmt.Errorf("One of the nodes is nil. n1: %v, n2: %v", n1, n2)
 	}
 	p1 = n1.findPath(&(t.root), []Node{})
-	p2 = n2.findPath(&(t.root), []Node{}) 
+	p2 = n2.findPath(&(t.root), []Node{})
 	var lcm Node
-	for i:=0; i<len(p1) && i<len(p2); i++ {
-		if p1[i].data == p2[i].data{
+	for i := 0; i < len(p1) && i < len(p2); i++ {
+		if p1[i].data == p2[i].data {
 			lcm = p1[i]
 		}
 	}
 	return lcm, nil
 }
-func (n *Node) findPath(other *Node, path []Node) []Node{
+func (n *Node) findPath(other *Node, path []Node) []Node {
 	if n.data == other.data {
 		path = append(path, *other)
 		return path
 	}
 	path = append(path, *other)
-	for _, node :=  range other.childs{
+	for _, node := range other.childs {
 		n.findPath(node, path)
-		if len(path)>0 && path[len(path)-1].data == n.data{
+		if len(path) > 0 && path[len(path)-1].data == n.data {
 			return path
 		}
 		path = path[:len(path)-1]
