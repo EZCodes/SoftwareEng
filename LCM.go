142025d3fe06a7a77509570a1aea12123908f02e
Jerzy Jaskuc <jaskucj@tcd.ie>
2019-10-04 22:27:13 +0100

    build working

--- LCM.go
+++ LCM.go
@@ -1,6 +1,7 @@
 package LCA
 
 import "fmt"
+import "math"
 
 type Graph struct {
 	vertices []*Vertex
@@ -31,21 +32,23 @@ func (g *Graph) LowestCommonAncestor(v1, v2 *Vertex) (Vertex, error) {
 		return Vertex{}, fmt.Errorf("One of the vertices is nil. v1: %v, v2: %v", v1, v2)
 	}
 	an1 = v1.findAncestors(0,v1,[]Ancestor{})
+
 	an2 = v2.findAncestors(0,v2,[]Ancestor{})
+
 	//TODO fix depth of itself, prob infinity
-	var lcm Vertex
+	lcm :=  Ancestor{data: &Vertex{data: math.MaxInt32}} // 32bits should be enough for this scale
 	for _, ancestorOne := range an1 {
 		for _, ancestorTwo := range an2 {
-			if ancestorTwo.data.data == acncestorOne.data.data {
-                                if ancestorTwo.depth < lcm.depth {
-                                        lcm = &(ancestorTwo.data)
-                                }
-                        }
+			if ancestorTwo.data.data == ancestorOne.data.data {
+      	if ancestorTwo.distance < lcm.distance {
+        	lcm = ancestorTwo
+        }
+			}
 		}
 	}
-	return lcm, nil
+	return *(lcm.data), nil
 }
-func (v *Vertex) findAncestors(d int,curentV *Vertex, ancestors []Ancestor) []Ancestor {
+func (v *Vertex) findAncestors(d int, currentV *Vertex, ancestors []Ancestor) []Ancestor {
 	ancestors = append(ancestors, Ancestor{
 		ancOf: v,
 		data : currentV,
@@ -56,7 +59,7 @@ func (v *Vertex) findAncestors(d int,curentV *Vertex, ancestors []Ancestor) []An
 	}
 	d++
 	for _, edge := range currentV.incEdg {
-		ancestors = v.findAncestors(edge.src, ancestors)
+		ancestors = v.findAncestors(d, edge.src, ancestors)
 	}
 	return ancestors
 }
