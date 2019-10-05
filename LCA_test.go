2624b12d5e6f917e65dc131a308a9c11852289a7
Jerzy Jaskuc <jaskucj@tcd.ie>
2019-10-05 19:06:31 +0100

    Fixed bugs, wrote tests, most of them pass

--- LCA_test.go
+++ LCA_test.go
@@ -5,212 +5,220 @@ import "testing"
 func TestLowestCommonAncestor(t *testing.T) {
 
 	tests := []struct {
-		tree    Tree
-		nodeOne *Node
-		nodeTwo *Node
-		want    Node
+		graph   Graph
+		vOne    *Vertex
+		vTwo    *Vertex
+		want    Vertex
 		wantErr bool
 	}{
 		{
 			// empty tree
-			nodeOne: nil,
-			nodeTwo: nil,
+			vOne:    nil,
+			vTwo:    nil,
 			wantErr: true,
 		}, {
-			// line tree
-			tree: Tree{
-				root: Node{
-					data: 1,
-					childs: []*Node{
-						&Node{
-							data: 2,
-							childs: []*Node{
-								&Node{
-									data: 3,
-									childs: []*Node{
-										&Node{
-											data: 4,
-											childs: []*Node{
-												&Node{
-													data:   5,
-													childs: []*Node{},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
+			graph: Graph{
+				edges: []*Edge{
+					&Edge{
+						dest: &Vertex{data: 2},
+						src:  &Vertex{data: 1},
+					},
+					&Edge{
+						dest: &Vertex{data: 3},
+						src:  &Vertex{data: 2},
+					},
+					&Edge{
+						dest: &Vertex{data: 4},
+						src:  &Vertex{data: 3},
+					},
+					&Edge{
+						dest: &Vertex{data: 5},
+						src:  &Vertex{data: 2},
+					},
+					&Edge{
+						dest: &Vertex{data: 6},
+						src:  &Vertex{data: 3},
+					},
+					&Edge{
+						dest: &Vertex{data: 7},
+						src:  &Vertex{data: 5},
+					},
+					&Edge{
+						dest: &Vertex{data: 7},
+						src:  &Vertex{data: 4},
 					},
 				},
 			},
-			nodeOne: &Node{
-				data: 3,
+			vOne: &Vertex{
+				data: 7,
 			},
-			nodeTwo: &Node{
-				data: 4,
-			},
-			wantErr: false,
-			want: Node{
-				data: 2,
-			},
-		}, {
-			// two sides of the root
-			tree: Tree{
-				root: Node{
-					data: 1,
-					childs: []*Node{
-						&Node{
-							data: 4,
-							childs: []*Node{
-								&Node{
-									data:   5,
-									childs: nil,
-								},
-							},
-						},
-						&Node{
-							data: 2,
-							childs: []*Node{
-								&Node{
-									data:   3,
-									childs: nil,
-								},
-							},
-						},
-					},
-				},
-			},
-			nodeOne: &Node{
-				data: 3,
-			},
-			nodeTwo: &Node{
-				data: 5,
-			},
-			wantErr: false,
-			want: Node{
-				data: 1,
-			},
-		}, {
-			//one side of the root
-			tree: Tree{
-				root: Node{
-					data: 1,
-					childs: []*Node{
-						&Node{
-							data:   4,
-							childs: nil,
-						},
-						&Node{
-							data: 2,
-							childs: []*Node{
-								&Node{
-									data: 3,
-									childs: []*Node{
-										&Node{
-											data:   5,
-											childs: nil,
-										},
-									},
-								},
-								&Node{
-									data:   6,
-									childs: nil,
-								},
-							},
-						},
-					},
-				},
-			},
-			nodeOne: &Node{
-				data: 5,
-			},
-			nodeTwo: &Node{
+			vTwo: &Vertex{
 				data: 6,
 			},
 			wantErr: false,
-			want: Node{
-				data: 2,
+			want: Vertex{
+				data: 3,
 			},
 		}, {
-			// more than 2 childs
-			tree: Tree{
-				root: Node{
-					data: 1,
-					childs: []*Node{
-						&Node{
-							data: 2,
-							childs: []*Node{
-								&Node{
-									data:   3,
-									childs: nil,
-								},
-								&Node{
-									data:   4,
-									childs: nil,
-								},
-								&Node{
-									data:   5,
-									childs: nil,
-								},
-							},
-						},
+			graph: Graph{
+				edges: []*Edge{
+					&Edge{
+						dest: &Vertex{data: 2},
+						src:  &Vertex{data: 1},
+					},
+					&Edge{
+						dest: &Vertex{data: 3},
+						src:  &Vertex{data: 1},
+					},
+					&Edge{
+						dest: &Vertex{data: 4},
+						src:  &Vertex{data: 2},
+					},
+					&Edge{
+						dest: &Vertex{data: 5},
+						src:  &Vertex{data: 3},
+					},
+					&Edge{
+						dest: &Vertex{data: 6},
+						src:  &Vertex{data: 3},
+					},
+					&Edge{
+						dest: &Vertex{data: 6},
+						src:  &Vertex{data: 2},
 					},
 				},
 			},
-			nodeOne: &Node{
-				data: 3,
+			vOne: &Vertex{
+				data: 6,
 			},
-			nodeTwo: &Node{
+			vTwo: &Vertex{
 				data: 4,
 			},
 			wantErr: false,
-			want: Node{
+			want: Vertex{
 				data: 2,
 			},
 		}, {
-			//node is not in the tree
-			tree: Tree{
-				root: Node{
-					data: 1,
-					childs: []*Node{
-						&Node{
-							data: 4,
-							childs: []*Node{
-								&Node{
-									data:   5,
-									childs: nil,
-								},
-							},
-						},
-						&Node{
-							data: 2,
-							childs: []*Node{
-								&Node{
-									data:   3,
-									childs: nil,
-								},
-							},
-						},
+			graph: Graph{
+				edges: []*Edge{
+					&Edge{
+						dest: &Vertex{data: 5},
+						src:  &Vertex{data: 1},
+					},
+					&Edge{
+						dest: &Vertex{data: 3},
+						src:  &Vertex{data: 1},
+					},
+					&Edge{
+						dest: &Vertex{data: 4},
+						src:  &Vertex{data: 2},
+					},
+					&Edge{
+						dest: &Vertex{data: 5},
+						src:  &Vertex{data: 2},
 					},
 				},
 			},
-			nodeOne: &Node{
+			vOne: &Vertex{
+				data: 5,
+			},
+			vTwo: &Vertex{
 				data: 3,
 			},
-			nodeTwo: &Node{
-				data: 7,
+			wantErr: false,
+			want: Vertex{
+				data: 1,
+			},
+		}, {
+			graph: Graph{
+				edges: []*Edge{
+					&Edge{
+						dest: &Vertex{data: 2},
+						src:  &Vertex{data: 1},
+					},
+					&Edge{
+						dest: &Vertex{data: 3},
+						src:  &Vertex{data: 2},
+					},
+					&Edge{
+						dest: &Vertex{data: 4},
+						src:  &Vertex{data: 3},
+					},
+					&Edge{
+						dest: &Vertex{data: 5},
+						src:  &Vertex{data: 4},
+					},
+				},
+			},
+			vOne: &Vertex{
+				data: 4,
+			},
+			vTwo: &Vertex{
+				data: 3,
+			},
+			wantErr: false,
+			want: Vertex{
+				data: 2,
+			},
+		}, {
+			//no common ancestor
+			graph: Graph{
+				edges: []*Edge{
+					&Edge{
+						dest: &Vertex{data: 3},
+						src:  &Vertex{data: 1},
+					},
+					&Edge{
+						dest: &Vertex{data: 4},
+						src:  &Vertex{data: 1},
+					},
+					&Edge{
+						dest: &Vertex{data: 4},
+						src:  &Vertex{data: 2},
+					},
+				},
+			},
+			vOne: &Vertex{
+				data: 2,
+			},
+			vTwo: &Vertex{
+				data: 3,
 			},
 			wantErr: false,
 
-			want: Node{
+			want: Vertex{
+				data: 0,
+			},
+		}, {
+			//node not in the graph
+			graph: Graph{
+				edges: []*Edge{
+					&Edge{
+						dest: &Vertex{data: 3},
+						src:  &Vertex{data: 1},
+					},
+					&Edge{
+						dest: &Vertex{data: 2},
+						src:  &Vertex{data: 1},
+					},
+				},
+			},
+			vOne: &Vertex{
+				data: 4,
+			},
+			vTwo: &Vertex{
+				data: 2,
+			},
+			wantErr: false,
+
+			want: Vertex{
 				data: 0,
 			},
 		},
 	}
 	for index, test := range tests {
 		t.Run(string(index), func(t *testing.T) {
-			fNode, err := test.tree.LowestCommonAncestor(test.nodeOne, test.nodeTwo)
+			fNode, err := test.graph.LowestCommonAncestor(test.vOne, test.vTwo)
 			if !(test.wantErr) && err != nil {
 				t.Errorf("Error has occured: %q", err)
 			}
--- LCM.go
+++ LCM.go
@@ -3,81 +3,107 @@ package LCA
 import "fmt"
 
 type Graph struct {
-	vertices []*Vertex
-	edges    []*Edge
+	edges []*Edge
 }
 type Vertex struct {
-	data   int
-	outEdg []*Edge
-	incEdg []*Edge
+	data int
 }
 type Edge struct {
 	dest *Vertex
 	src  *Vertex
 }
 type Ancestor struct {
-	ancOf *Vertex
-	data *Vertex
+	data     *Vertex
 	distance int
 }
 
 // LowestCommonAncestor find the lowest common ancestor of two nodes
 // if found, the node with it is returned, if two nodes dont have a common ancestor
-// empty node is returned
+// empty vertex (value 0) is returned
 func (g *Graph) LowestCommonAncestor(v1, v2 *Vertex) (Vertex, error) {
 	var an1 []Ancestor
 	var an2 []Ancestor
 	if v1 == nil || v2 == nil {
 		return Vertex{}, fmt.Errorf("One of the vertices is nil. v1: %v, v2: %v", v1, v2)
 	}
-	an1 = v1.findAncestors(0,v1,[]Ancestor{})
+	an1 = v1.findAncestors(0, v1, []Ancestor{}, g.edges)
 	an1 = an1[1:] // last element is itself
-	an2 = v2.findAncestors(0,v2,[]Ancestor{})
-	an2 = an2[1:] // last element is itsef, we dont count node itself as own ancestor
-	distanceToDepth(an1) // only need to do this with one list since if they have LCA, max depth will be same
-	lcm :=  Ancestor{distance: 0} // 32bits should be enough for this scale
+	an2 = v2.findAncestors(0, v2, []Ancestor{}, g.edges)
+	an2 = an2[1:]        // last element is itsef, we dont count node itself as own ancestor
+	an1 = distanceToDepth(an1) // only need to do this with one list since if they have LCA, max depth will be same
+	lcm := Ancestor{
+		distance: 0,
+		data:     &Vertex{data: 0},
+	}
+
 	for _, ancestorOne := range an1 {
 		for _, ancestorTwo := range an2 {
 			if ancestorTwo.data.data == ancestorOne.data.data {
-      	if ancestorOne.distance > lcm.distance {
-        	lcm = ancestorOne
-        }
+				if ancestorOne.distance > lcm.distance {
+					lcm = ancestorOne
+				}
 			}
 		}
 	}
 	return *(lcm.data), nil
 }
-func (v *Vertex) findAncestors(d int, currentV *Vertex, ancestors []Ancestor) []Ancestor {
-	ancestors = append(ancestors, Ancestor{
-		ancOf: v,
-		data : currentV,
+func (v *Vertex) findAncestors(d int, currentV *Vertex, ancestors []Ancestor, edges []*Edge) []Ancestor {
+	currAncestor := Ancestor{
+		data:     currentV,
 		distance: d,
-	})	
-	if len(currentV.incEdg) == 0 {
+	}
+	if !contains(currAncestor, ancestors) {
+		ancestors = append(ancestors, currAncestor)
+	}
+	directAncestors := currentV.findSources(edges)
+	if len(directAncestors) == 0 {
 		return ancestors
 	}
 	d++
-	for _, edge := range currentV.incEdg {
-		ancestors = v.findAncestors(d, edge.src, ancestors)
+	for _, vertex := range directAncestors {
+		ancestors = v.findAncestors(d, vertex, ancestors, edges)
 	}
 	return ancestors
 }
 
+func (v *Vertex) findSources(edges []*Edge) []*Vertex {
+	var sources []*Vertex
+	for _, edge := range edges {
+		if v.data == edge.dest.data {
+			sources = append(sources, edge.src)
+		}
+	}
+	return sources
+}
+
 // convert distance from node of interest into depth from the "root"
 // basically a reverse
-func distanceToDepth (ancestors []Ancestor) []Ancestor {
+func distanceToDepth(ancestors []Ancestor) []Ancestor {
 	currentDepth := 1
+	var result []Ancestor
 	maxDepth := findMax(ancestors)
-	for ;maxDepth > 0; {
+	for maxDepth > 0 {
 		for _, ancestor := range ancestors {
 			if ancestor.distance == maxDepth {
-				ancestor.distance = currentDepth
+				newAncestor := ancestor			
+				newAncestor.distance = currentDepth
+				result = append(result, newAncestor)
 			}
 		}
 		currentDepth++
 		maxDepth--
 	}
-	return ancestors
+	return result
+}
+
+func contains(ancestor Ancestor, ancestors []Ancestor) bool {
+	contained := false
+	for _, other := range ancestors {
+		if ancestor.data.data == other.data.data {
+			contained = true
+		}
+	}
+	return contained
 }
 
 func findMax(ancestors []Ancestor) int {
